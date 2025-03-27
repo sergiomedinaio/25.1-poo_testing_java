813ab8b78af6845a55794870d595e3d5ae704845
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-03-27 09:55:12 -0300

    feat: add Automovil class instance

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!\n");
+        Automovil automovil = new Automovil();
+        automovil.color = "red";
+        automovil.cantidadRuedas = 4;
+        automovil.latitud = 121.1f;
+        automovil.longitud = 121.1f;
+        automovil.medio = "terrestre";
+        automovil.nombre = "Pequeño bandido";
+
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
